3a69b5233bc62dbe1ca54483f54078b53dd73ee1
KarenQiao <498101094@qq.com>
2017-12-06 13:48:26 +0800

    提交合并福利集市样式

--- src/component/welfare_market_new/homepage/banner.js
+++ src/component/welfare_market_new/homepage/banner.js
@@ -4,30 +4,4 @@
 
 
 
-var marketMoreFn={
 
-    showFn:function(){
-        var allBar=document.getElementsByClassName('fixed_side_btn')[0];
-
-        if(allBar.className.indexOf('showMore')>-1){
-
-            allBar.className=allBar.className.replace('showMore','')
-
-        }else {
-            allBar.className='fixed_side_btn showMore';
-        }
-    },
-
-
-    hideFn:function(){
-        var allBar=document.getElementsByClassName('fixed_side_btn')[0];
-
-        if(allBar.className.indexOf('showMore')>-1){
-
-            allBar.className=allBar.className.replace('showMore','')
-
-        }
-
-    }
-
-};
